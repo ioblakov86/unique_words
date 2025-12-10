cec940e56f439d86fe78d4bd0ad807aa266b8be0
ioblakov86 <144415410+ioblakov86@users.noreply.github.com>
2025-12-10 21:44:06 +0300

    initial version

---
+++ go.mod
@@ -0,0 +1,3 @@
+module unique_words
+
+go 1.22.0
---
+++ unique_words.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+	"unicode"
+)
+
+func getStats(text string) (map[string]int, []string, []string, error) {
+	if text == "" {
+		return nil, nil, nil, fmt.Errorf("текст пустой")
+	}
+
+	text = strings.ToLower(text)
+	f := func(c rune) bool {
+		return !unicode.IsLetter(c) && !unicode.IsNumber(c)
+	}
+	words := strings.FieldsFunc(text, f)
+
+	fmt.Println(words)
+
+	uniqueWords := make(map[string]int)
+
+	for _, word := range words {
+		count, ok := uniqueWords[word]
+		if ok {
+			uniqueWords[word] = count + 1
+		} else {
+			uniqueWords[word] = 1
+		}
+	}
+
+	keys := make([]string, 0, len(uniqueWords))
+	for key := range uniqueWords {
+		keys = append(keys, key)
+	}
+
+	fmt.Println(uniqueWords)
+	fmt.Println(keys)
+
+	sort.SliceStable(keys, func(i, j int) bool {
+		return uniqueWords[keys[i]] > uniqueWords[keys[j]]
+	})
+	return uniqueWords, keys[:min(5, len(keys))], words, nil
+}
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Введите многострочный текст (конец - пустая строка):")
+
+	var lines []string
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			break
+		}
+		lines = append(lines, line)
+	}
+
+	text := strings.Join(lines, " ")
+	uniqueWordsCount, top5, words, err := getStats(text)
+	if err != nil {
+		fmt.Println("Ошибка:", err)
+		return
+	}
+	fmt.Println("\nОбщее число слов:", len(words))
+	fmt.Println("\nЧисло уникальных слов:", len(uniqueWordsCount))
+	fmt.Println("\nТоп-5 самых частых слов:")
+	for _, word := range top5 {
+		fmt.Println("  ", word, " - ", uniqueWordsCount[word])
+	}
+}
